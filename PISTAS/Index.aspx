56004d21bb19d8c02044a38914091a8dd6b02331
Kevin Tapias <kevin-1134@live.com>
2018-08-10 21:58:24 -0500

    Juego con javascript

---
+++ PISTAS/Index.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="PISTAS.Index" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Juego by Kvn</title>
+    <link href="Public/css.css" rel="stylesheet" type="text/css" />
+     <link rel="icon" href="RES/anchor.png" type="image/gif" sizes="16x16">
+</head>
+<body>
+    <div class="container">
+        <h1>Juego! <h3 id="fin"></h3></h1>
+        <h4>Encuentra los elementos segun las pistas - Tienes 1 Minuto</h4>
+        <img src="RES/ship-2089747_960_720.jpg" class="mainImg" usemap="elementsMap" />
+        <map name="elementsMap">
+            <!--VELA-->
+            <area id="obj1" class="obj" coords="0,100,203,90" shape="poly"  onclick="found1();" />
+             <!--PROA-->
+            <area id="obj2" class="obj" target="" title="" coords="158,484,30" shape="circle" onclick="found2();"/>
+             <!--REMOS-->
+            <area id="obj3" class="obj" target="" title="" shape="circle" onclick="found3();"/>
+        </map>
+        <div class="tiempos">
+            <label>Tiempo:</label>
+            <label class="count" id="contador">0</label><span>s</span>
+        </div>
+        <div class="opciones">
+            <button onclick="getPista();" id="getPista">Siguiente pista!</button>
+            <button onclick="reload();">Reiniciar juego</button>
+        </div>
+        <div class="msg">
+            <label>Pista #<span id="#pista">1</span></label>
+        <label id="pista" class="pista #"></label>
+        </div>
+    </div>
+</body>
+     <script src="Public/js.js"></script>
+</html>
---
+++ PISTAS/Public/js.js
@@ -0,0 +1,76 @@
+﻿const elm1 = document.getElementById("obj1");
+const elm2 = document.getElementById("obj2");
+const elm3 = document.getElementById("obj3");
+const btnPista = document.getElementById("getPista");
+var pst = document.getElementById("pista");
+var end = document.getElementById("fin");
+var counter = 0;
+var npista = document.getElementById("#pista");
+
+var crono;
+
+function reload() {
+    location.reload(true);
+}
+
+//Crono
+function tiempo() {
+    crono = setInterval(function () {
+        txt = document.getElementById("contador");
+        counter++;
+        txt.innerHTML = counter;
+        if (counter >= 60) {
+            //cuando llegue al minuto stop & alerta
+            clearInterval(crono);
+            end.innerHTML = "El juego ha terminado";
+            disableAll();
+        }
+    }, 1000);
+    elm1.style.display = "block";
+    btnPista.disabled = true;
+}
+
+tiempo();
+
+/*mostrar pistas 1*/
+var x = 0;
+function getPista() {
+    var pistas1 = ["Mantiene el equilibrio del bote",
+        "Corta las aguas al navegar",
+        "Con estos empujamos en bote"];
+    pst.innerHTML = pistas1[x];
+    x = x + 1;
+    if (x == 2) {
+        btnPista.disabled = true;
+    }
+    btnPista.disabled = true;
+    npista.innerHTML = x;
+}
+getPista();
+
+function found1() {
+    elm2.style.display = "block";
+    btnPista.disabled = false;
+    pst.innerHTML = "Muy Bien! es la Vela";
+    elm1.style.display = "none";
+}
+
+function found2() {
+    elm3.style.display = "block";
+    btnPista.disabled = false;
+    pst.innerHTML = "Muy Bien! es la Proa";
+    elm2.style.display = "none";
+}
+
+function found3() {
+    clearInterval(crono);
+    pst.innerHTML = "Muy Bien! son los Remos";
+    end.innerHTML = "El juego ha terminado";
+}
+
+function disableAll() {
+    elm1.style.display = "none";
+    elm2.style.display = "none";
+    elm3.style.display = "none";
+    btnPista.disabled = true;
+}
